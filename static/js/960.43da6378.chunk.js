328b8a80aa0b53432a872eceb4b8c36a06a5035f
IhorMelnyk90 <91090917+IhorMelnyk90@users.noreply.github.com>
2022-11-06 23:17:41 +0000

    Deploying to gh-pages from @ IhorMelnyk90/goit-react-hw-08-phonebook@e9958604d98f1a424b7f274661c0eaa9f3ab10c8 🚀

---
+++ static/js/960.43da6378.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[960],{8960:function(e,n,o){o.r(n),o.d(n,{default:function(){return c}});var t=o(1087),r=o(184);var c=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h2",{children:"Not found"}),(0,r.jsx)(t.rU,{to:"/",children:"Back to home"})]})}}}]);
+//# sourceMappingURL=960.43da6378.chunk.js.map
